46d31ba249370cb871a6f6387b6cec79697f58e8
Alex Pham <67773092+apalex@users.noreply.github.com>
2024-02-08 11:43:32 -0500

    Add files via upload

---
+++ Lab/PhamAlexandreLab01/ArithmeticOperations/ArithmeticOperations/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program to do simple arithmetic
+            int number1, number2, sum, difference,
+                product, quotient, remainder;
+            String firstName;
+            string lastName;
+            String line3, line4, line5, line6, line7;
+
+            Console.Write("Please Enter your Last Name: ");
+            lastName = Console.ReadLine();
+            Console.Write("Please Enter your First Name: ");
+            firstName = Console.ReadLine();
+            Console.Write("Please Enter First Integer Number: ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("Please Enter Second Integer Number: ");
+            number2 = int.Parse(Console.ReadLine());
+
+            sum = number1 + number2;
+            difference = number1 - number2;
+            product = number1 * number2;
+            quotient = number1 / number2;
+            remainder = number1 % number2;
+
+            Console.WriteLine($"Your First Name is : {firstName}");
+            Console.WriteLine($"Your First Name is : {lastName}");
+            Console.WriteLine($"The sum of {number1} and {number2} is {sum}");
+            Console.WriteLine($"The difference between {number1} and {number2} is {difference}");
+            Console.WriteLine($"The product of {number1} and {number2} is {product}");
+            Console.WriteLine($"The quotient of {number1} divided by {number2} is {quotient}");
+            Console.WriteLine($"The remainder of {number1} and {number2} is {remainder}");
+
+            line3 = "The sum of " + number1 + " and " + number2 + " is";
+            line4 = "The difference between " + number1 + " and " + number2 + " is";
+            line5 = "The product of " + number1 + " and " + number2 + " is";
+            line6 = "The quotient of " + number1 + " divided by " + number2 + " is";
+            line7 = "The remainder of " + number1 + " divided by " + number2 + " is";
+
+            drawLine();
+            Console.WriteLine($"| {"Table of Arithmetic Operations", 53}  {"|", 25}");
+            drawLine();
+            Console.WriteLine($"| {"Your First Name is", -50} | {firstName, -25} |");
+            Console.WriteLine($"| {"Your Last Name is", -50} | {lastName, -25} |");
+            Console.WriteLine($"| {line3, -50} | {sum, 25} |");
+            Console.WriteLine($"| {line4, -50} | {difference, 25} |");
+            Console.WriteLine($"| {line5, -50} | {product, 25} |");
+            Console.WriteLine($"| {line6, -50} | {quotient, 25} |");
+            Console.WriteLine($"| {line7, -50} | {remainder, 25} |");
+            drawLine();
+        }
+        public static void drawLine()
+        {
+            Console.Write("|");
+            for (int i = 0; i < 80; i++)
+                Console.Write("-");
+            Console.WriteLine("|");
+        }
+    }
+}
