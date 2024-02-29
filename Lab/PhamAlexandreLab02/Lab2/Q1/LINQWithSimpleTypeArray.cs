631eedaba08e608f3275c0279aa16a66a2fb92f7
Alex Pham <67773092+apalex@users.noreply.github.com>
2024-02-29 14:23:59 -0500

    Add files via upload

---
+++ Lab/PhamAlexandreLab02/Lab2/Q1/LINQWithSimpleTypeArray.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class LINQWithSimpleTypeArray
+    {
+        static void Main(string[] args)
+        {
+            var values = new[] { 2, 9, 5, 0, 3, 7, 1, 4, 8, 5 };
+
+            Console.Write("Original array:");
+            foreach (var value in values)
+            {
+                Console.Write($" {value}");
+            }
+
+            // LINQ Query
+            var filtered =
+                from value in values
+                where value > 4
+                select value;
+
+            Console.Write("\nArray values greater than 4:");
+            foreach (var value in filtered)
+            {
+                Console.Write($" {value}");
+            }
+
+            var sorted =
+                from value in values
+                orderby value
+                select value;
+
+            Console.Write("\nOriginal array, sorted:");
+            foreach (var value in sorted)
+            {
+                Console.Write($" {value}");
+            }
+
+            var sortFilteredResults =
+                from value in filtered
+                orderby value descending
+                select value;
+
+            Console.Write("\nValues greater than 4, descending order (two queries):");
+            foreach (var value in sortFilteredResults)
+            {
+                Console.Write($" {value}");
+            }
+
+            var sortAndFilter =
+                from value in values
+                where value > 4
+                orderby value descending
+                select value;
+
+            Console.Write("\nValues greater than 4, descending order (one query):");
+            foreach (var value in sortAndFilter)
+            {
+                Console.Write($" {value}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lab/PhamAlexandreLab02/Lab2/Q2/LINQWithArrayOfObjects.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    class LINQWithArrayOfObjects
+    {
+        static void Main(string[] args)
+        {
+            var employees = new[]
+            {
+                new Employee("Jason", "Red", 5000M),
+                new Employee("Ashely", "Green", 7600M),
+                new Employee("Matthew", "Indigo", 3587.5M),
+                new Employee("James", "Indigo", 4700.77M),
+                new Employee("Luke", "Indigo", 6200M),
+                new Employee("Jason", "Blue", 3200M),
+                new Employee("Wendy", "Brown", 4236.4M)
+            };
+
+            // Display all employees
+            Console.WriteLine("Original array:");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+
+            // Filter salaries
+            var between4K6K =
+                from employee in employees
+                where (employee.MonthlySalary >= 4000M) && (employee.MonthlySalary <= 6000M)
+                select employee;
+
+            // Display Employees making between 4000 and 6000 per month
+            Console.WriteLine("\nEmployees earning in the range " + $"{4000:C}-{6000:C} per month:");
+            foreach (var employee in between4K6K)
+            {
+                Console.WriteLine(employee);
+            }
+
+            // Order Employees by last name, then first name
+            var nameSorted =
+                from employee in employees
+                orderby employee.LastName, employee.FirstName
+                select employee;
+
+            Console.WriteLine("\nFirst employee when sorted by name:");
+            if (nameSorted.Any())
+            {
+                Console.WriteLine(nameSorted.First());
+            }
+            else
+            {
+                Console.WriteLine("not found");
+            }
+
+            // Select Last Names
+            var lastNames =
+                from employee in employees
+                select employee.LastName;
+
+            Console.WriteLine("\nUnique employee last names:");
+            foreach (var employee in lastNames.Distinct())
+            {
+                Console.WriteLine(employee);
+            }
+
+            // Select First and Last names
+            var names =
+                from employee in employees
+                select new { employee.FirstName, employee.LastName };
+
+            Console.WriteLine("\nNames only:");
+            foreach (var employee in names)
+            {
+                Console.WriteLine(employee);
+            }
+            Console.WriteLine();
+        }
+    }
+}
