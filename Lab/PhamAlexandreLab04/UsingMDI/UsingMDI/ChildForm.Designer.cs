e9d54a4e7bb8686e3e5b64596e81adbb39ae62fa
Alex Pham <67773092+apalex@users.noreply.github.com>
2024-04-01 10:40:10 -0400

    Add files via upload

---
+++ Lab/PhamAlexandreLab04/UsingMDI/UsingMDI/ChildForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace UsingMDI
+{
+    partial class ChildForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.displayPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // displayPictureBox
+            // 
+            this.displayPictureBox.Image = global::UsingMDI.Properties.Resources.yellowflowers;
+            this.displayPictureBox.Location = new System.Drawing.Point(31, 42);
+            this.displayPictureBox.Name = "displayPictureBox";
+            this.displayPictureBox.Size = new System.Drawing.Size(472, 314);
+            this.displayPictureBox.TabIndex = 0;
+            this.displayPictureBox.TabStop = false;
+            // 
+            // ChildForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 397);
+            this.Controls.Add(this.displayPictureBox);
+            this.Name = "ChildForm";
+            this.Text = "ChildForm";
+            ((System.ComponentModel.ISupportInitialize)(this.displayPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox displayPictureBox;
+    }
+}
---
+++ Lab/PhamAlexandreLab04/UsingMDI/UsingMDI/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace UsingMDI
+{
+    partial class UsingMDIForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lavenderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.purpleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yellowFlowersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.windowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cascadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tileHorizontalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tileVerticalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lavenderFlowersToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.purpleFlowersToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.yellowFlowersToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.windowsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(862, 36);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 30);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lavenderToolStripMenuItem,
+            this.purpleToolStripMenuItem,
+            this.yellowFlowersToolStripMenuItem});
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(149, 34);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // lavenderToolStripMenuItem
+            // 
+            this.lavenderToolStripMenuItem.Name = "lavenderToolStripMenuItem";
+            this.lavenderToolStripMenuItem.Size = new System.Drawing.Size(250, 34);
+            this.lavenderToolStripMenuItem.Text = "Lavender Flowers";
+            this.lavenderToolStripMenuItem.Click += new System.EventHandler(this.lavenderToolStripMenuItem_Click);
+            // 
+            // purpleToolStripMenuItem
+            // 
+            this.purpleToolStripMenuItem.Name = "purpleToolStripMenuItem";
+            this.purpleToolStripMenuItem.Size = new System.Drawing.Size(250, 34);
+            this.purpleToolStripMenuItem.Text = "Purple Flowers";
+            this.purpleToolStripMenuItem.Click += new System.EventHandler(this.purpleToolStripMenuItem_Click);
+            // 
+            // yellowFlowersToolStripMenuItem
+            // 
+            this.yellowFlowersToolStripMenuItem.Name = "yellowFlowersToolStripMenuItem";
+            this.yellowFlowersToolStripMenuItem.Size = new System.Drawing.Size(250, 34);
+            this.yellowFlowersToolStripMenuItem.Text = "Yellow Flowers";
+            this.yellowFlowersToolStripMenuItem.Click += new System.EventHandler(this.yellowFlowersToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(149, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // windowsToolStripMenuItem
+            // 
+            this.windowsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cascadeToolStripMenuItem,
+            this.tileHorizontalToolStripMenuItem,
+            this.tileVerticalToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.lavenderFlowersToolStripMenuItem1,
+            this.purpleFlowersToolStripMenuItem1,
+            this.yellowFlowersToolStripMenuItem1});
+            this.windowsToolStripMenuItem.Name = "windowsToolStripMenuItem";
+            this.windowsToolStripMenuItem.Size = new System.Drawing.Size(102, 30);
+            this.windowsToolStripMenuItem.Text = "Windows";
+            // 
+            // cascadeToolStripMenuItem
+            // 
+            this.cascadeToolStripMenuItem.Name = "cascadeToolStripMenuItem";
+            this.cascadeToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.cascadeToolStripMenuItem.Text = "Cascade";
+            this.cascadeToolStripMenuItem.Click += new System.EventHandler(this.cascadeToolStripMenuItem_Click);
+            // 
+            // tileHorizontalToolStripMenuItem
+            // 
+            this.tileHorizontalToolStripMenuItem.Name = "tileHorizontalToolStripMenuItem";
+            this.tileHorizontalToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.tileHorizontalToolStripMenuItem.Text = "Tile Horizontal";
+            this.tileHorizontalToolStripMenuItem.Click += new System.EventHandler(this.tileHorizontalToolStripMenuItem_Click);
+            // 
+            // tileVerticalToolStripMenuItem
+            // 
+            this.tileVerticalToolStripMenuItem.Name = "tileVerticalToolStripMenuItem";
+            this.tileVerticalToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.tileVerticalToolStripMenuItem.Text = "Tile Vertical";
+            this.tileVerticalToolStripMenuItem.Click += new System.EventHandler(this.tileVerticalToolStripMenuItem_Click);
+            // 
+            // lavenderFlowersToolStripMenuItem1
+            // 
+            this.lavenderFlowersToolStripMenuItem1.CheckOnClick = true;
+            this.lavenderFlowersToolStripMenuItem1.Name = "lavenderFlowersToolStripMenuItem1";
+            this.lavenderFlowersToolStripMenuItem1.Size = new System.Drawing.Size(270, 34);
+            this.lavenderFlowersToolStripMenuItem1.Text = "1 Lavender Flowers";
+            // 
+            // purpleFlowersToolStripMenuItem1
+            // 
+            this.purpleFlowersToolStripMenuItem1.CheckOnClick = true;
+            this.purpleFlowersToolStripMenuItem1.Name = "purpleFlowersToolStripMenuItem1";
+            this.purpleFlowersToolStripMenuItem1.Size = new System.Drawing.Size(270, 34);
+            this.purpleFlowersToolStripMenuItem1.Text = "2 Purple Flowers";
+            // 
+            // yellowFlowersToolStripMenuItem1
+            // 
+            this.yellowFlowersToolStripMenuItem1.CheckOnClick = true;
+            this.yellowFlowersToolStripMenuItem1.Name = "yellowFlowersToolStripMenuItem1";
+            this.yellowFlowersToolStripMenuItem1.Size = new System.Drawing.Size(270, 34);
+            this.yellowFlowersToolStripMenuItem1.Text = "3 Yellow Flowers";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(267, 6);
+            // 
+            // UsingMDIForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 676);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "UsingMDIForm";
+            this.Text = "Using MDI";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lavenderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem purpleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yellowFlowersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem windowsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cascadeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tileHorizontalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tileVerticalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lavenderFlowersToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem purpleFlowersToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem yellowFlowersToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
