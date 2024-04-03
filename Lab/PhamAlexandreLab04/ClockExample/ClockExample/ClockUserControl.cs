ed92e4f2972498936a5aba50f0a585f90823a0ed
Alex Pham <67773092+apalex@users.noreply.github.com>
2024-04-02 22:31:52 -0400

    Add files via upload

---
+++ Lab/PhamAlexandreLab04/ClockExample/ClockExample/ClockUserControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ClockExample
+{
+    public partial class ClockUserControl : UserControl
+    {
+        public ClockUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void clockTimer_Tick(object sender, EventArgs e)
+        {
+            displayLabel.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
