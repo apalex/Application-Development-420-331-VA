5571c43571cba4975ebb81687476c7f482e1916f
Alex Pham <67773092+apalex@users.noreply.github.com>
2024-05-03 07:48:40 -0400

    Assignment04
    Assignment04 commit

---
+++ Assignments/PhamAlexandreAssignment04/Assignment04/Assignment04/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Assignment04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.txtBoxRegion = new System.Windows.Forms.TextBox();
+            this.txtBoxCoatOfArms = new System.Windows.Forms.TextBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(67, 328);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(164, 41);
+            this.btnPrevious.TabIndex = 0;
+            this.btnPrevious.Text = "Previous Record";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(248, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(162, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Game of Thrones Houses API";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Region";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(94, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Coat Of Arms";
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.Location = new System.Drawing.Point(219, 119);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(298, 22);
+            this.txtBoxName.TabIndex = 6;
+            // 
+            // txtBoxRegion
+            // 
+            this.txtBoxRegion.Location = new System.Drawing.Point(219, 172);
+            this.txtBoxRegion.Name = "txtBoxRegion";
+            this.txtBoxRegion.Size = new System.Drawing.Size(298, 22);
+            this.txtBoxRegion.TabIndex = 7;
+            // 
+            // txtBoxCoatOfArms
+            // 
+            this.txtBoxCoatOfArms.Location = new System.Drawing.Point(219, 239);
+            this.txtBoxCoatOfArms.Name = "txtBoxCoatOfArms";
+            this.txtBoxCoatOfArms.Size = new System.Drawing.Size(298, 22);
+            this.txtBoxCoatOfArms.TabIndex = 8;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(427, 328);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(164, 41);
+            this.btnNext.TabIndex = 9;
+            this.btnNext.Text = "Next Record";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 450);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.txtBoxCoatOfArms);
+            this.Controls.Add(this.txtBoxRegion);
+            this.Controls.Add(this.txtBoxName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnPrevious);
+            this.Name = "Form1";
+            this.Text = "JSON RESTful API Ice and Fire";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.TextBox txtBoxRegion;
+        private System.Windows.Forms.TextBox txtBoxCoatOfArms;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
+
---
+++ Assignments/PhamAlexandreAssignment04/Assignment04/Assignment04/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Assignment04
+{
+    public partial class Form1 : Form
+    {
+        private HttpClient client;
+        private int currentIndex = 0;
+        private const string api = "https://anapioficeandfire.com/api/houses/";
+
+        public Form1()
+        {
+            client = new HttpClient();
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private async void btnNext_Click(object sender, EventArgs e)
+        {
+            currentIndex++;
+            await LoadHouse(currentIndex);
+        }
+
+        private async void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (currentIndex > 0 && currentIndex != 1)
+            {
+                currentIndex--;
+                await LoadHouse(currentIndex);
+            } else if (currentIndex == 1)
+            {
+                txtBoxName.Text = "";
+                txtBoxRegion.Text = "";
+                txtBoxCoatOfArms.Text = "";
+                currentIndex--;
+            }
+        }
+
+        private async Task LoadHouse(int index)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(api + index);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                House house = JsonConvert.DeserializeObject<House>(responseBody);
+                DisplayHouse(house);
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"Error fetching house: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DisplayHouse(House house)
+        {
+            txtBoxName.Text = house.Name;
+            txtBoxRegion.Text = house.Region;
+            txtBoxCoatOfArms.Text = house.CoatOfArms;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+    public class House
+    {
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string CoatOfArms { get; set; }
+    }
+}
